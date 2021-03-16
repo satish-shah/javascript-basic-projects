5b9488544fb3cdd0b80a9d58316593c44d077d7e
satish-shah <fireside.satish2012@gmail.com>
2021-03-17 10:44:25 +1100

    random color updated
    color for simple component wa updated

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  //Getting random number btw 0-3
+  const randonNumber = getRandomNumber();
+  console.log(randonNumber);
+  document.body.style.backgroundColor = colors[randonNumber];
+  color.textContent = colors[randonNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
